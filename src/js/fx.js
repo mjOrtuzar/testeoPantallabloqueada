af996b4f1cd53ae0411939ed11f86516d627c36f
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-09-30 22:18:14 -0300

    testeo con mediaquery

--- src/js/fx.js
+++ src/js/fx.js
@@ -1,16 +1,6 @@
 
 $(document).ready(function(){
-    $(window).bind('orientationchange resize', function(event){
-        if (event.orientation) {
-          if (event.orientation == 'landscape') {
-            if (window.rotation == 90) {
-              rotate(this, -90);
-            } else {
-              rotate(this, 90);
-            }
-          }
-        }
-      });
+    
       
 
     
