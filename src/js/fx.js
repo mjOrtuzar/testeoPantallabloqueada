9276313bee7e60160f207d13e8bc66462f245153
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-09-30 22:10:29 -0300

    probando api js

--- src/js/fx.js
+++ src/js/fx.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
     function reorient(e) {
         var portrait = (window.orientation % 180 == 0);
-        $("body > section").css("-webkit-transform", !portrait ? "rotate(90deg)" : "");
+        $("body > section").css("-webkit-transform", !landscape ? "rotate(90deg)" : "");
     }
     window.onorientationchange = reorient;
     window.setTimeout(reorient, 0);
