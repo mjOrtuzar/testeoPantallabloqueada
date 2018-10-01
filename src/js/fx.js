054a07d6b489d4b1f707f5540c1a6b719ac79580
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-09-30 21:22:45 -0300

    first-commit

---
+++ src/js/fx.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    function reorient(e) {
+        var portrait = (window.orientation % 180 == 0);
+        $("body > section").css("-webkit-transform", !portrait ? "rotate(90deg)" : "");
+    }
+    window.onorientationchange = reorient;
+    window.setTimeout(reorient, 0);
+    
+
+    
+
+});
